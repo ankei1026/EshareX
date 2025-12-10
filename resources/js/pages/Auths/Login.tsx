69c0388643952d3d4ad3a7ca83cc89f2acb2631e
Errol Sabanal <sabanal.errol12@gmail.com>
2025-12-10 19:26:57 +0800

    feat(login): added login page without functionality

--- resources/js/pages/Auths/Login.tsx
+++ resources/js/pages/Auths/Login.tsx
@@ -11,31 +11,66 @@ import {
     FieldTitle,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input";
-import { Switch } from "@headlessui/react";
+import Title from "../Components/Title";
+import { useForm } from "@inertiajs/react";
+import password from "@/routes/password";
+import { Button } from "@/components/ui/button";
 
 const Login = () => {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        username: '',
+        password: '',
+    })
+
+    const handleLogin = (e: React.FormEvent) => {
+        e.preventDefault();
+    }
+
     return (
-        <div className="flex items-center justify-center mt-94">
-            <FieldSet>
-                <FieldLegend>Profile</FieldLegend>
-                <FieldDescription>This appears on invoices and emails.</FieldDescription>
-                <FieldGroup>
-                    <Field>
-                        <FieldLabel htmlFor="name">Full name</FieldLabel>
-                        <Input id="name" autoComplete="off" placeholder="Evil Rabbit" />
-                        <FieldDescription>This appears on invoices and emails.</FieldDescription>
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="username">Username</FieldLabel>
-                        <Input id="username" autoComplete="off" aria-invalid />
-                        <FieldError>Choose another username.</FieldError>
-                    </Field>
-                    <Field orientation="horizontal">
-                        <Switch id="newsletter" />
-                        <FieldLabel htmlFor="newsletter">Subscribe to the newsletter</FieldLabel>
-                    </Field>
-                </FieldGroup>
-            </FieldSet>
+        <div className="flex justify-center items-center h-screen">
+            <form className="w-full  lg:w-1/2 flex items-center justify-center" onSubmit={handleLogin}>
+                <FieldSet className="w-100">
+                    <Title>Login to EshareX</Title>
+                    <FieldDescription>This appears on invoices and emails.</FieldDescription>
+                    <FieldGroup>
+                        <Field>
+                            <FieldLabel htmlFor="username">Full name</FieldLabel>
+                            <Input
+                                id="username"
+                                type="name"
+                                autoComplete="off"
+                                placeholder="ankei1026"
+                                required
+                                onChange={(e) => setData('username', e.target.value)}
+                                value={data.username} />
+                            <FieldError>{errors.username}</FieldError>
+                        </Field>
+                        <Field>
+                            <FieldLabel htmlFor="password">Password</FieldLabel>
+                            <Input
+                                id="password"
+                                type="password"
+                                autoComplete="off"
+                                required
+                                onChange={(e) => setData('password', e.target.value)}
+                                aria-invalid />
+                            <FieldError>{errors.password}</FieldError>
+                        </Field>
+                    </FieldGroup>
+                    <Button
+                        className="w-full"
+                        type="submit"
+                        onClick={handleLogin}
+                        disabled={processing}
+                    >
+                        Login
+                    </Button>
+                </FieldSet>
+
+            </form>
+            <div className="w-1/2 h-full hidden lg:block dark:bg-white bg-black">
+                <h1 className="text-black">Hello</h1>
+            </div>
         </div>
     )
 }
